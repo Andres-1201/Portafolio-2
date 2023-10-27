9ddd14bc96194b0ccdaa470b89a2bdd60e5dae8a
H3AVY-69 <oesandoval3@soy.sena.edu.co>
2023-10-27 11:50:44 -0500

    cambios

--- js/sneakers2.js
+++ js/sneakers2.js
@@ -10,12 +10,15 @@ let thumb1=document.getElementById("thumb1");
 let thumb2=document.getElementById("thumb2");
 let thumb3=document.getElementById("thumb3");
 let thumb4=document.getElementById("thumb4");
-let botonCerrar =document.getElementById("botonCerrar")
+let botonCerrar =document.getElementById("botonCerrar");
+let boxMinus =document.getElementById("boxMinus");
+let boxCantidad =document.getElementById("boxCantidad");
+let boxPlus =document.getElementById("boxPlus");
+let precioUnitario = document.getElementById("PrecioUnitario");
+let valorUnitario = 180000;
+let valorTotal = document.getElementById("valorTotal");
 
-botonCerrar.addEventListener("click", function() {
-    boxImgFixed.classList.add("box-img-fixed-hidden");
-    boxImgFixed.classList.remove("box-img-fixed");
-});
+
 
 mainImg.addEventListener("click", function() {
    boxImgFixed.classList.remove("box-img-fixed-hidden");
@@ -83,3 +86,30 @@ thumb3.addEventListener("click", function () {
     mainImgFixed3.classList.add("img-hidden");
     mainImgFixed4.classList.remove("img-hidden");
   });
+
+  botonCerrar.addEventListener("click", function() {
+    boxImgFixed.classList.add("box-img-fixed-hidden");
+    boxImgFixed.classList.remove("box-img-fixed");
+});
+
+let valorUnitarioAux = Intl.NumberFormat("DE-de").format(valorUnitario)
+document.getElementById("precioUnitario").innerHTML = "$" + valorUnitarioAux
+
+  boxMinus.addEventListener("click", function() {
+    let cantidad =boxcantidad.innerHTML;
+    if (cantidad > 0) {
+        cantidad--;
+        document.getElementById("boxcantidad").innerHTML = cantidad;
+        let valorTotalInt = cantidad * valorUnitario;
+        let valorTotalAux = Intl.NumberFormat("DE-de").format(valorTotalInt);
+        document.getElementById("valorTotal").innerHTML = "$" + valorTotalAux
+    }
+});
+
+boxPlus.addEventListener("click", function() {
+    let cantidad =boxcantidad.innerHTML;
+    if (cantidad < 10) {
+        cantidad++;
+        document.getElementById("boxcantidad").innerHTML = cantidad;
+    }
+});
