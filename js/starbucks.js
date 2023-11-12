63564de0b23c4987e793fdc0299f8e79e212f60c
daniel22alejandro <danniel10yara@gmail.com>
2023-11-12 17:23:03 -0500

    add más aaaahh

--- js/starbucks.js
+++ js/starbucks.js
@@ -8,19 +8,19 @@ iceGreen.addEventListener("mouseover", function(){
     mainCircle.classList.add("main-circle-green")
     mainCircle.classList.remove("main-circle-rose")
     mainCircle.classList.remove("main-circle-pink")
-    mainImg.setAttribute("src", "img/img1.png")
+    mainImg.setAttribute("src", "imag/img1.png")
 })
 
 iceRose.addEventListener("mouseover", function(){
     mainCircle.classList.remove("main-circle-green")
     mainCircle.classList.add("main-circle-rose")
     mainCircle.classList.remove("main-circle-pink")
-    mainImg.setAttribute("src", "img/img2.png")
+    mainImg.setAttribute("src", "imag/img2.png")
 })
 
 icePink.addEventListener("mouseover", function(){
     mainCircle.classList.remove("main-circle-green")
     mainCircle.classList.remove("main-circle-rose")
     mainCircle.classList.add("main-circle-pink")
-    mainImg.setAttribute("src", "img/img3.png")
+    mainImg.setAttribute("src", "imag/img3.png")
 })
